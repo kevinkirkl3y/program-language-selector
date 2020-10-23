241e3c1ec17a08b4eafd5a35e48dd1526c339ae3
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-23 09:27:41 -0700

    Added range slider, troubleshooting how to access input.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $("button#submit").click(function(){
+    
+    
+    const dindinStr = document.getElementById("din-din")
+    const dindin = parseInt(dindinStr);
+    console.log(dindin);
+  
+  
+  
+  
+  event.preventDefault();
+  });
+
+
+
+
+
+});
