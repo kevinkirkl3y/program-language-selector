0d0b455090532d520fbc6e56a8c55dc856b40d56
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-23 10:37:25 -0700

    Completed script to collect user input and create integers in order to be used in branch later.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,29 @@ $(document).ready(function(){
   $("button#submit").click(function(){
     
     
-    let dindinStr = document.getElementById("dinner")
-    const dindinVal = parseInt(dindinStr.value);
+    let dinnerStr = document.getElementById("dinner")
+    const dinnerVal = parseInt(dinnerStr.value);
 
+    let coffeeStr = document.getElementById("coffee")
+    const coffeeVal = parseInt(coffeeStr.value);
+
+    let musicStr = document.getElementById("music")
+    const musicVal = parseInt(musicStr.value);
+
+    let biglebowskiStr = document.getElementById("biglebowski")
+    const biglebowskiVal = parseInt(biglebowskiStr.value);
+
+    let bestStr = document.getElementById("best")
+    const bestVal = parseInt(bestStr.value);
+
+    const result =dinnerVal + coffeeVal + musicVal + biglebowskiVal + bestVal;
 
     
     
-    console.log(dindinVal);
-    
-    const result =dindinVal +
     
     
+    
+    console.log(result);
     
     
   
